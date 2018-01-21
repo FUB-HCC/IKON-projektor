6ea645075c3df688abe20123e3a8dbb56864ded9
Constantin Eissler <constantineissler@MBP-Windows.fritz.box>
2018-01-21 11:04:48 +0100

    IP2 ejected npm/ restructured folders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import classes from './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Conny
-        </p>
+      <div className={classes.AppIntro}>
+          <p className={classes.Ikon}>IKON</p>
       </div>
     );
   }
