7a2408ffbb0e43dbb6f6f1ceab422a75e659c8b3
Constantin Eissler <constantineissler@MBP-Windows.fritz.box>
2018-01-21 13:32:21 +0100

    IP2 Graphs render conditional

---
+++ src/components/TimeGraph/TimeGraph.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import tGraph from '../../assets/Process.png'
+
+const timeGraph = (props) => {
+  return (
+    <div><img src={tGraph}/></div>
+  )
+}
+
+export default timeGraph
--- src/containers/GraphView/GraphView.js
+++ src/containers/GraphView/GraphView.js
@@ -1,10 +1,26 @@
 import React, {Component} from 'react'
 import PetridishGraph from '../../components/PetridishGraph/PetridishGraph'
+import AreaGraph from '../../components/AreaGraph/AreaGraph'
+import TimeGraph from '../../components/TimeGraph/TimeGraph'
 import classes from './GraphView.css'
 
 class GraphView extends Component {
   render () {
-    const Graph = (<PetridishGraph/>) // render conditional according to state
+    let Graph = 1 // render conditional according to state
+    switch (Graph) {
+      case 0:
+        Graph = (<PetridishGraph/>)
+        break
+      case 1:
+        Graph = (<TimeGraph/>)
+        break
+      case 2:
+        Graph = (<AreaGraph/>)
+        break
+      default:
+        Graph = null
+    }
+
     return (
       <div className={classes.BackGradient}>
         <div className={classes.GraphContainer}>
