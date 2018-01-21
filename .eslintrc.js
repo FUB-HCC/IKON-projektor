640d7c14dce0edc53382510f0b2ba89641ab35be
Constantin Eissler <constantineissler@MBP-Windows.fritz.box>
2018-01-21 11:25:54 +0100

    configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: ["standard", "plugin:react/recommended"],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+    env: {
+      browser: 1,
+      jasmine: true
+    },
+    plugins: ["react"],
+    rules:{
+      "react/display-name": "off",
+      "react/prop-types": "off",
+      "no-new-wrappers": "off",
+      "no-var": 2
+    },
+    globals: {
+      "_": false // Exclude warnings when using underscore.js
+    }
+  }
--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
 import classes from './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className={classes.AppIntro}>
-          <p className={classes.Ikon}>IKON</p>
+        <p className={classes.Ikon}>IKON</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
