c9ae7cb5c3b26f99d75026d89f2aae79025d3f9f
Simon Buchholz <simonbuchholz91@gmail.com>
2018-07-16 14:58:21 +0200

    - Conditionally include Geographies (when clicking on an Institute Marker all corresponding research area geographies are rendered) - Integrating React-motion for smooth zooming and panning

--- src/components/Visualizations/AreaChart/AreaChart.js
+++ src/components/Visualizations/AreaChart/AreaChart.js
@@ -7,8 +7,11 @@ import {
   Geographies,
   Geography,
   Marker,
-  Markers
+  Markers,
+  Lines,
+  Line
 } from 'react-simple-maps'
+import { Motion, spring } from 'react-motion'
 
 const wrapperStyles = {
   width: '100%',
@@ -16,37 +19,46 @@ const wrapperStyles = {
   margin: '0 auto',
   fontFamily: 'Roboto, sans-serif'
 }
-const sampleData = [
-  {name: 'Tokyo', coordinates: [139.6917, 35.6895], numberProjects: 37843000},
-  {name: 'Jakarta', coordinates: [106.8650, -6.1751], numberProjects: 30539000},
-  {name: 'Delhi', coordinates: [77.1025, 28.7041], numberProjects: 24998000},
-  {name: 'Manila', coordinates: [120.9842, 14.5995], numberProjects: 24123000},
-  {name: 'Seoul', coordinates: [126.9780, 37.5665], numberProjects: 23480000},
-  {name: 'Shanghai', coordinates: [121.4737, 31.2304], numberProjects: 23416000},
-  {name: 'Karachi', coordinates: [67.0099, 24.8615], numberProjects: 22123000},
-  {name: 'Beijing', coordinates: [116.4074, 39.9042], numberProjects: 21009000},
-  {name: 'New York', coordinates: [-74.0059, 40.7128], numberProjects: 20630000},
-  {name: 'Guangzhou', coordinates: [113.2644, 23.1291], numberProjects: 20597000},
-  {name: 'Sao Paulo', coordinates: [-46.6333, -23.5505], numberProjects: 20365000},
-  {name: 'Mexico City', coordinates: [-99.1332, 19.4326], numberProjects: 20063000},
-  {name: 'Mumbai', coordinates: [72.8777, 19.0760], numberProjects: 17712000},
-  {name: 'Osaka', coordinates: [135.5022, 34.6937], numberProjects: 17444000},
-  {name: 'Moscow', coordinates: [37.6173, 55.7558], numberProjects: 16170000},
-  {name: 'Dhaka', coordinates: [90.4125, 23.8103], numberProjects: 15669000},
-  {name: 'Greater Cairo', coordinates: [31.2357, 30.0444], numberProjects: 15600000},
-  {name: 'Los Angeles', coordinates: [-118.2437, 34.0522], numberProjects: 15058000},
-  {name: 'Bangkok', coordinates: [100.5018, 13.7563], numberProjects: 14998000},
-  {name: 'Kolkata', coordinates: [88.3639, 22.5726], numberProjects: 14667000},
-  {name: 'Buenos Aires', coordinates: [-58.3816, -34.6037], numberProjects: 14122000},
-  {name: 'Tehran', coordinates: [51.3890, 35.6892], numberProjects: 13532000},
-  {name: 'Istanbul', coordinates: [28.9784, 41.0082], numberProjects: 13287000},
-  {name: 'Lagos', coordinates: [3.3792, 6.5244], numberProjects: 13123000},
-  {name: 'Shenzhen', coordinates: [114.0579, 22.5431], numberProjects: 12084000},
-  {name: 'Rio de Janeiro', coordinates: [-43.1729, -22.9068], numberProjects: 11727000},
-  {name: 'Kinshasa', coordinates: [15.2663, -4.4419], numberProjects: 11587000},
-  {name: 'Tianjin', coordinates: [117.3616, 39.3434], numberProjects: 10920000},
-  {name: 'Paris', coordinates: [2.3522, 48.8566], numberProjects: 10858000},
-  {name: 'Lima', coordinates: [-77.0428, -12.0464], numberProjects: 10750000}
+const sampleInstitutes = [
+  {name: 'Tokyo',
+    coordinates: [139.6917, 35.6895],
+    numberProjects: 378,
+    researchAreas: [ // Country codes as ISO_A3
+      {area: 'RUS', number: 100},
+      {area: 'STP', number: 100},
+      {area: 'EGY', number: 50},
+      {area: 'SOM', number: 50},
+      {area: 'CAF', number: 78}
+    ]},
+  {name: 'Jakarta', coordinates: [106.8650, -6.1751], numberProjects: 305},
+  {name: 'Delhi', coordinates: [77.1025, 28.7041], numberProjects: 249},
+  {name: 'Manila', coordinates: [120.9842, 14.5995], numberProjects: 241},
+  {name: 'Seoul', coordinates: [126.9780, 37.5665], numberProjects: 234},
+  {name: 'Shanghai', coordinates: [121.4737, 31.2304], numberProjects: 234},
+  {name: 'Karachi', coordinates: [67.0099, 24.8615], numberProjects: 221},
+  {name: 'Beijing', coordinates: [116.4074, 39.9042], numberProjects: 210},
+  {name: 'New York', coordinates: [-74.0059, 40.7128], numberProjects: 206},
+  {name: 'Guangzhou', coordinates: [113.2644, 23.1291], numberProjects: 205},
+  {name: 'Sao Paulo', coordinates: [-46.6333, -23.5505], numberProjects: 203},
+  {name: 'Mexico City', coordinates: [-99.1332, 19.4326], numberProjects: 200},
+  {name: 'Mumbai', coordinates: [72.8777, 19.0760], numberProjects: 177},
+  {name: 'Osaka', coordinates: [135.5022, 34.6937], numberProjects: 174},
+  {name: 'Moscow', coordinates: [37.6173, 55.7558], numberProjects: 161},
+  {name: 'Dhaka', coordinates: [90.4125, 23.8103], numberProjects: 156},
+  {name: 'Greater Cairo', coordinates: [31.2357, 30.0444], numberProjects: 156},
+  {name: 'Los Angeles', coordinates: [-118.2437, 34.0522], numberProjects: 150},
+  {name: 'Bangkok', coordinates: [100.5018, 13.7563], numberProjects: 149},
+  {name: 'Kolkata', coordinates: [88.3639, 22.5726], numberProjects: 146},
+  {name: 'Buenos Aires', coordinates: [-58.3816, -34.6037], numberProjects: 141},
+  {name: 'Tehran', coordinates: [51.3890, 35.6892], numberProjects: 135},
+  {name: 'Istanbul', coordinates: [28.9784, 41.0082], numberProjects: 132},
+  {name: 'Lagos', coordinates: [3.3792, 6.5244], numberProjects: 131},
+  {name: 'Shenzhen', coordinates: [114.0579, 22.5431], numberProjects: 120},
+  {name: 'Rio de Janeiro', coordinates: [-43.1729, -22.9068], numberProjects: 117},
+  {name: 'Kinshasa', coordinates: [15.2663, -4.4419], numberProjects: 115},
+  {name: 'Tianjin', coordinates: [117.3616, 39.3434], numberProjects: 109},
+  {name: 'Paris', coordinates: [2.3522, 48.8566], numberProjects: 108},
+  {name: 'Lima', coordinates: [-77.0428, -12.0464], numberProjects: 107}
 ]
 
 class AreaChart extends Component {
@@ -54,15 +66,22 @@ class AreaChart extends Component {
     super()
     this.state = {
       zoom: 1,
+      center: [0, 20],
       geographyPaths: [],
       institutions: [],
-      zoomableGroup: null
+      zoomableGroup: null,
+      includedAreas: []
     }
     this.loadPaths = this.loadPaths.bind(this)
     this.handleZoom = this.handleZoom.bind(this)
     this.handleCountryClick = this.handleCountryClick.bind(this)
     this.handleMarkerClick = this.handleMarkerClick.bind(this)
     this.handlerSrollMap = this.handlerSrollMap.bind(this)
+    this.buildCurves = this.buildCurves.bind(this)
+  }
+
+  updateData () {
+
   }
 
   componentDidMount () {
@@ -76,17 +95,27 @@ class AreaChart extends Component {
         const world = res.data
         // Transform your paths with topojson however you want...
         const countries = feature(world, world.objects[Object.keys(world.objects)[0]]).features
-        this.setState({geographyPaths: countries, institutions: sampleData})
+        this.setState({geographyPaths: countries, institutions: sampleInstitutes})
         this.zoomableGroup.zoomableGroupNode.addEventListener('wheel', this.handlerSrollMap)
       })
   }
 
-  handleCountryClick (countryIndex) {
-    console.log('Clicked on country: ', countryIndex)
+  handleCountryClick (country) {
+    console.log('Clicked on country: ', country)
   }
 
-  handleMarkerClick (markerIndex) {
-    console.log('Marker: ', markerIndex)
+  handleMarkerClick (marker) {
+    let newIncludedAreas = []
+    if (marker.researchAreas) {
+      for (let researchArea of marker.researchAreas) {
+        newIncludedAreas.push(researchArea.area)
+      }
+    }
+    this.setState({
+      zoom: 2,
+      center: marker.coordinates,
+      includedAreas: newIncludedAreas
+    })
   }
 
   handlerSrollMap (scrollEvent) {
@@ -111,89 +140,155 @@ class AreaChart extends Component {
     console.log('New center: ', newCenter)
   }
 
+  // This funtion returns a curve command that builds a quadratic curve.
+  // And depending on the line's curveStyle property it curves in one direction or the other.
+  buildCurves (start, end, line) {
+    const x0 = start[0]
+    const x1 = end[0]
+    const y0 = start[1]
+    const y1 = end[1]
+    const curve = {
+      forceUp: `${x1} ${y0}`,
+      forceDown: `${x0} ${y1}`
+    }[line.curveStyle]
+
+    return `M ${start.join(' ')} Q ${curve} ${end.join(' ')}`
+  }
+
   render () {
     return (
       <div style={wrapperStyles}>
         <button onClick={() => this.handleZoom(1.25)}>{'Zoom in'}</button>
         <button onClick={() => this.handleZoom(1 / 1.25)}>{'Zoom out'}</button>
         <hr/>
-        <ComposableMap
-          projectionConfig={{
-            scale: 160,
-            rotation: [0, 0, 0]
+        <Motion
+          defaultStyle={{
+            zoom: 1,
+            x: 0,
+            y: 20
           }}
-          width={980}
-          height={551}
           style={{
-            width: '100%',
-            height: 'auto'
+            zoom: spring(this.state.zoom, {stiffness: 210, damping: 20}),
+            x: spring(this.state.center[0], {stiffness: 210, damping: 20}),
+            y: spring(this.state.center[1], {stiffness: 210, damping: 20})
           }}
         >
-          <ZoomableGroup
-            onMoveStart={this.handleMoveStart}
-            onMoveEnd={this.handleMoveEnd}
-            onwheel={this.handlerSrollMap}
-            ref={(node) => { this.zoomableGroup = node }}
-            zoom={this.state.zoom}>
-            <Geographies geography={this.state.geographyPaths} disableOptimization>
-              {(geographies, projection) =>
-                geographies.map((geography, i) =>
-                  <Geography
-                    key={`${geography.properties.ADM0_A3}-${i}`}
-                    cacheId={`path-${geography.properties.ADM0_A3}-${i}`}
-                    onClick={() => this.handleCountryClick(i)}
-                    round
-                    geography={geography}
-                    projection={projection}
-                    style={{
-                      default: {
-                        fill: '#ECEFF1',
-                        stroke: '#607D8B',
-                        strokeWidth: 0.75,
-                        outline: 'none'
-                      },
-                      hover: {
-                        fill: '#607D8B',
-                        stroke: '#607D8B',
-                        strokeWidth: 0.75,
-                        outline: 'none'
-                      },
-                      pressed: {
-                        fill: '#FF5722',
-                        stroke: '#607D8B',
-                        strokeWidth: 0.75,
-                        outline: 'none'
+          {({zoom, x, y}) => (
+            <ComposableMap
+              projectionConfig={{
+                scale: 205,
+                rotation: [0, 0, 0]
+              }}
+              width={980}
+              height={551}
+              style={{
+                width: '100%',
+                height: 'auto'
+              }}
+            >
+              <ZoomableGroup
+                onMoveStart={this.handleMoveStart}
+                onMoveEnd={this.handleMoveEnd}
+                onwheel={this.handlerSrollMap}
+                ref={(node) => { this.zoomableGroup = node }}
+                center={[x, y]}
+                zoom={zoom}>
+                <Geographies geography={this.state.geographyPaths} disableOptimization>
+                  {(geographies, projection) =>
+                    geographies.map((geography, i) =>
+                      <Geography
+                        key={`${geography.properties.ADM0_A3}-${i}`}
+                        cacheId={`path-${geography.properties.ADM0_A3}-${i}`}
+                        onClick={this.handleCountryClick}
+                        round
+                        geography={geography}
+                        projection={projection}
+                        style={{
+                          default: {
+                            fill: '#ECEFF1',
+                            stroke: '#607D8B',
+                            strokeWidth: 0.75,
+                            outline: 'none'
+                          },
+                          hover: {
+                            fill: '#607D8B',
+                            stroke: '#607D8B',
+                            strokeWidth: 0.75,
+                            outline: 'none'
+                          },
+                          pressed: {
+                            fill: '#ff418b',
+                            stroke: '#607D8B',
+                            strokeWidth: 0.75,
+                            outline: 'none'
+                          }
+                        }}
+                      />
+                    )}
+                </Geographies>
+                <Geographies geography={this.state.geographyPaths} disableOptimization>
+                  {(geographies, projection) =>
+                    geographies.map((geography, i) => {
+                      return this.state.includedAreas.indexOf(geography.properties.ISO_A3) !== -1 && (
+                        <Geography
+                          key={`include-${geography.properties.ADM0_A3}-${i}`}
+                          cacheId={`include-${geography.properties.ADM0_A3}-${i}`}
+                          onClick={this.handleCountryClick}
+                          round
+                          geography={geography}
+                          projection={projection}
+                          style={{
+                            default: {
+                              fill: '#91403c',
+                              stroke: '#607D8B',
+                              strokeWidth: 0.75,
+                              outline: 'none'
+                            }
+                          }}
+                        />
+                      )
+                    })
+                  }
+                </Geographies>
+                <Markers>
+                  {this.state.institutions.map((institution, i) =>
+                    <Marker
+                      key={`institution-marker-${i}`}
+                      marker={institution}
+                      onClick={this.handleMarkerClick}
+                      style={{
+                        default: {
+                          fill: 'rgba(255,87,34,0.8)',
+                          stroke: '#FFFFFF'
+                        },
+                        hover: {
+                          fill: '#4b9123',
+                          stroke: '#2e2e2e'
+                        },
+                        pressed: {
+                          fill: '#918c45',
+                          stroke: '#2e2e2e'
+                        }
+                      }}>
+                      <circle cx={0} cy={0} r={this.state.zoom * institution.numberProjects / 30}/>
+                    </Marker>
+                  )}
+                </Markers>
+                <Lines>
+                  <Line
+                    line={{
+                      coordinates: {
+                        start: [0, 0],
+                        end: [-99.1, 19.4]
                       }
                     }}
+                    buildPath={this.buildCurves}
                   />
-                )}
-            </Geographies>
-            <Markers>
-              {this.state.institutions.map((institution, i) =>
-                <Marker
-                  key={`institution-marker-${i}`}
-                  marker={institution}
-                  onClick={() => this.handleMarkerClick(i)}
-                  style={{
-                    default: {
-                      fill: '#91403c',
-                      stroke: '#FFFFFF'
-                    },
-                    hover: {
-                      fill: '#4b9123',
-                      stroke: '#2e2e2e'
-                    },
-                    pressed: {
-                      fill: '#918c45',
-                      stroke: '#2e2e2e'
-                    }
-                  }}>
-                  <circle cx={0} cy={0} r={this.state.zoom * institution.numberProjects / 3000000}/>
-                </Marker>
-              )}
-            </Markers>
-          </ZoomableGroup>
-        </ComposableMap>
+                </Lines>
+              </ZoomableGroup>
+            </ComposableMap>
+          )}
+        </Motion>
       </div>
     )
   }
