9ffeeae5dd823b24851b25ad6f7e28aac484152f
Simon Buchholz <simonbuchholz91@gmail.com>
2018-08-08 18:07:35 +0200

    Timeline shows first Forschungsbereich ("Lebenswissenschaften")

--- src/components/Visualizations/TimeLine/TimeLine.js
+++ src/components/Visualizations/TimeLine/TimeLine.js
@@ -2,76 +2,111 @@ import 'd3-transition'
 import React, {Component} from 'react'
 import SVGWithMargin from './SVGWithMargin'
 import styles from './TimeLine.css'
+import {getFieldColor} from '../../../store/utility'
 
 // Import the D3 libraries we'll be using for the spark line.
-import { extent as d3ArrayExtent } from 'd3-array'
+import {extent as d3ArrayExtent} from 'd3-array'
 import {
   scaleLinear as d3ScaleLinear,
   scaleTime as d3ScaleTime
 } from 'd3-scale'
-import { line as d3Line } from 'd3-shape'
+import {line as d3Line} from 'd3-shape'
 // Import the D3 libraries we'll use for the axes.
 import {
   axisBottom as d3AxisBottom,
   axisLeft as d3AxisLeft
 } from 'd3-axis'
-import { select as d3Select } from 'd3-selection'
-
-const sampleData = [
-  { 'day': '2017-04-18', 'productPerceivedQuality': '2.8' },
-  { 'day': '2017-04-19', 'productPerceivedQuality': '2.9' },
-  { 'day': '2017-04-20', 'productPerceivedQuality': '2.7' },
-  { 'day': '2017-04-21', 'productPerceivedQuality': '4.3' },
-  { 'day': '2017-04-22', 'productPerceivedQuality': '4.6' },
-  { 'day': '2017-04-23', 'productPerceivedQuality': '5' },
-  { 'day': '2017-04-24', 'productPerceivedQuality': '5.2' },
-  { 'day': '2017-04-25', 'productPerceivedQuality': '5.1' },
-  { 'day': '2017-04-26', 'productPerceivedQuality': '4.8' },
-  { 'day': '2017-04-27', 'productPerceivedQuality': '4.9' },
-  { 'day': '2017-04-28', 'productPerceivedQuality': '5.1' },
-  { 'day': '2017-04-29', 'productPerceivedQuality': '5.3' },
-  { 'day': '2017-04-30', 'productPerceivedQuality': '5.6' },
-  { 'day': '2017-05-01', 'productPerceivedQuality': '6.2' }
-]
+import {select as d3Select} from 'd3-selection'
 
 class TimeLine extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      data: sampleData,
+      dataSplitYears: [],
+      forschungsbereiche: [],
       height: props.height * 0.5,
       width: props.width * 0.5,
-      margin: props.margin
+      margin: props.margin,
+      firstUpdate: true
     }
     // this.loadPaths = this.loadPaths.bind(this)
   }
 
   updateTimeGraph (data, height, width, margin) {
+    if (!this.state.firstUpdate) { // little workaround for first time scaling
+      this.setState({height: height * 0.5, width: width * 0.50, margin: margin})
+    }
     console.log(data)
-    this.setState({height: height * 0.5, width: width * 0.50, margin: margin})
+
+    let dataSplitYears = []
+    for (let projectsKey in data.projects) {
+      if (this.state.forschungsbereiche.indexOf(data.projects[projectsKey].forschungsbereichstr) === -1) this.state.forschungsbereiche.push(data.projects[projectsKey].forschungsbereichstr)
+
+      if (!(data.projects[projectsKey].forschungsbereichstr in dataSplitYears)) {
+        dataSplitYears[data.projects[projectsKey].forschungsbereichstr] = []
+      }
+
+      let startDate = parseInt(data.projects[projectsKey].start)
+      let endDate = parseInt(data.projects[projectsKey].end)
+      if (isNaN(endDate) || endDate === '') endDate = new Date().getFullYear()
+
+      let forschungsbereichData = dataSplitYears[data.projects[projectsKey].forschungsbereichstr]
+
+      let year = startDate
+      while (year <= endDate) {
+        let yearExists = false
+        for (let yearFbIndex in forschungsbereichData) { // check if year already is existent for the forschungsbereich
+          if (forschungsbereichData[yearFbIndex].year === year) {
+            yearExists = true
+            forschungsbereichData[yearFbIndex].numberOfActiveProjects = forschungsbereichData[yearFbIndex].numberOfActiveProjects + 1
+            forschungsbereichData[yearFbIndex].projects.push(data.projects[projectsKey])
+          }
+        }
+        if (!yearExists) {
+          forschungsbereichData.push(
+            {
+              year: year,
+              fb: data.projects[projectsKey].forschungsbereichstr,
+              numberOfActiveProjects: 1,
+              projects: [data.projects[projectsKey]],
+              color: getFieldColor(data.projects[projectsKey].forschungsbereichstr)
+            })
+        }
+
+        year++
+      }
+      dataSplitYears[data.projects[projectsKey].forschungsbereichstr] = forschungsbereichData.sort((a, b) => a.year - b.year)
+    }
+    console.log(dataSplitYears)
+    this.setState({dataSplitYears: dataSplitYears, projectsData: data, firstUpdate: false})
   }
 
   render () {
-    const selectY = datum => datum.productPerceivedQuality
-    const selectX = datum => new Date(datum.day).setHours(0, 0, 0, 0)
+    let fbKey = this.state.forschungsbereiche[1]
+    if (!fbKey || this.state.dataSplitYears[fbKey].length === 0) return null
+    let fbYearData = this.state.dataSplitYears[fbKey]
+    console.log(fbYearData)
+
+    const selectY = datum => datum.numberOfActiveProjects
+    const selectX = datum => new Date(datum.year.toString()).setHours(0, 0, 0, 0)
 
     // Since this is "time series" visualization, our x axis should have a time scale.
     // Our x domain will be the extent ([min, max]) of x values (Dates) in our data set.
     // Our x range will be from x=0 to x=width.
     const xScale = d3ScaleTime()
-      .domain(d3ArrayExtent(this.state.data, selectX))
+      .domain(d3ArrayExtent(fbYearData, selectX))
       .range([0, this.state.width])
 
     // Our y axis should just have a linear scale.
     // Our y domain will be the extent of y values (numbers) in our data set.
     const yScale = d3ScaleLinear()
-      .domain(d3ArrayExtent(this.state.data, selectY))
+      .domain(d3ArrayExtent(fbYearData, selectY))
       .range([this.state.height, 0])
 
     // Add an axis for our x scale which has half as many ticks as there are rows in the data set.
     const xAxis = d3AxisBottom()
       .scale(xScale)
-      .ticks(this.state.data.length / 2)
+      .ticks(fbYearData.length)
     // Add an axis for our y scale that has 3 ticks (FIXME: we should probably make number of ticks per axis a prop).
     const yAxis = d3AxisLeft()
       .scale(yScale)
@@ -86,14 +121,16 @@ class TimeLine extends Component {
       .x(selectScaledX)
       .y(selectScaledY)
 
-      // Create a line path of for our data.
-    const linePath = sparkLine(this.state.data)
+    // Create a line path of for our data.
+    const linePath = sparkLine(fbYearData)
 
     // map our data to scaled points.
-    const circlePoints = this.state.data.map(datum => ({
+    const circlePoints = fbYearData.map(datum => ({
       x: selectScaledX(datum),
       y: selectScaledY(datum)
     }))
+
+    console.log('testl')
 
     return (
       <SVGWithMargin
@@ -113,10 +150,15 @@ class TimeLine extends Component {
             transform: `translateY(${this.state.height}px)`
           }}
         />
-        <g className={styles.yAxis} ref={node => d3Select(node).call(yAxis)} />
+        <g className={styles.yAxis} ref={node => d3Select(node).call(yAxis)}/>
 
         <g className={styles.line}>
-          <path d={linePath} />
+          {/* TODO color setting does not work. */}
+          <path style={{
+            default: {
+              stroke: fbYearData.color
+            }
+          }} d={linePath}/>
         </g>
 
         {/* a group for our scatter plot, and render a circle at each `circlePoint`. */}
@@ -125,7 +167,7 @@ class TimeLine extends Component {
             <circle
               cx={circlePoint.x}
               cy={circlePoint.y}
-              key={`${circlePoint.x},${circlePoint.y}`}
+              key={`${Math.random()}${circlePoint.x},${circlePoint.y}`}
               r={4}
             />
           ))}
--- src/containers/GraphView/TimeLine.js
+++ src/containers/GraphView/TimeLine.js
@@ -5,11 +5,11 @@ import {default as TimeLineGraph} from '../../components/Visualizations/TimeLine
 
 class TimeLine extends React.Component {
   componentDidMount () {
-    this.Graph.updateTimeGraph(this.props.data, this.props.width, this.props.height, 20)
+    this.Graph.updateTimeGraph({dataSplitFb: this.props.dataSplitFb, projects: this.props.projects}, this.props.width, this.props.height, 20)
   }
 
   componentDidUpdate () {
-    this.Graph.updateTimeGraph(this.props.data, this.props.height, this.props.width, 20)
+    this.Graph.updateTimeGraph({dataSplitFb: this.props.dataSplitFb, projects: this.props.projects}, this.props.height, this.props.width, 20)
   }
 
   render () {
@@ -28,7 +28,8 @@ const graphColors = {
 const mapStateToProps = state => {
   const processedData = processData(state.main.filteredData, graphColors)
   return {
-    data: processedData,
+    dataSplitFb: processedData,
+    projects: state.main.filteredData,
     target: 'graph',
     colors: graphColors
   }
@@ -45,8 +46,8 @@ const processData = (data, colors) => {
 
    (Possibly split up into a different function for each Visualisation type)
  */
-  // Stage 1 create baseData and split by FB
-  if (!data) return []
+  // create baseData and split by FB
+  if (!data) return [[], [], [], []]
   let splitFbs = [[], [], [], []]
 
   Object.keys(data).forEach(pId => {
@@ -56,68 +57,12 @@ const processData = (data, colors) => {
       startDate: new Date(data[pId].start),
       endDate: new Date(data[pId].end),
       projectId: pId,
-      foundFit: false // needed to optimize Spacing (later deleted)
+      project: data[pId]
     }
     splitFbs[data[pId].forschungsbereichNumber - 1].push(d)
   })
 
-  // Stage 2 Optimize space for each fb and insert spacing between fbs
-  let resultData = []
-  let previousNums = 0
-  for (let i = 0; i < splitFbs.length; i++) {
-    // concat didnot work :(
-    let result = shuffleArray(optimizeSpace(splitFbs[i], previousNums))
-    for (let j = 0; j < result.length; j++) {
-      resultData.push(result[j])
-    }
-    previousNums += splitFbs[i].length
-    for (let j = 0; j < 5 && i < splitFbs.length - 1; j++) {
-      resultData.push({num: previousNums, startDate: null, endDate: null, projectId: null})
-      previousNums += 1
-    }
-  }
-  return resultData
-}
-
-const optimizeSpace = (data, offset) => {
-  /*
-      Group Projects into the same Row to minimize the width
-      by giving them the same num value.
-      (Restricted to 2 Projects per row with at least 1 Month between them )
-        data - preprocessed projects data
-        offset - int increasing all nums of this dataset
-          This is used to avoid overlapping num values between datasets.
-    */
-  data.sort(endDateSort)
-  for (let i = 0; i < data.length; i++) {
-    if (!data[i].foundFit) {
-      data[i].num = i + offset
-      for (let j = i + 1; j < data.length; j++) {
-        if (!data[j].foundFit && data[i].endDate.getTime() + (31 * 24 * 60 * 60 * 1000) <
-            data[j].startDate.getTime()) {
-          data[j].num = i + offset
-          data[j].foundFit = true
-          break
-        }
-      }
-    }
-    delete data[i].foundFit
-  }
-  return data
-}
-
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1))
-    let temp = array[i]
-    array[i] = array[j]
-    array[j] = temp
-  }
-  return array
-}
-
-const endDateSort = (a, b) => {
-  return new Date(a.endDate).getTime() - new Date(b.endDate).getTime()
+  return splitFbs
 }
 
 export default connect(mapStateToProps)(TimeLine)
