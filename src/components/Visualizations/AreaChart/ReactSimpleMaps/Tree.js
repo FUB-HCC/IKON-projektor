cc622c01196e8015f395401db7247bd5dba473fe
Simon Buchholz <simonbuchholz91@gmail.com>
2018-08-30 16:44:36 +0200

    Tree component supports click events now and 2 lines of projects

--- src/components/Visualizations/AreaChart/ReactSimpleMaps/Tree.js
+++ src/components/Visualizations/AreaChart/ReactSimpleMaps/Tree.js
@@ -8,12 +8,23 @@ class Tree extends Component {
       tree: props.tree,
       treeData: {}
     }
+    this.clickNode = this.clickNode.bind(this)
   }
 
   shouldComponentUpdate (nextProps, nextState) {
     // TODO only render when necessary
     let result = !Object.is(this.state, nextState)
     return result
+  }
+
+  clickNode (d) {
+    const {
+      onNodeClick,
+      projection
+    } = this.props
+    d.coordinates = projection.invert([d.x, d.y])
+    console.log(d)
+    return onNodeClick && onNodeClick(d, projection.invert([d.x, d.y]))
   }
 
   componentDidMount () {
@@ -23,9 +34,9 @@ class Tree extends Component {
       'children': []
     }
 
-    this.state.tree.projects.map((value) => {
-      let newValue = Object.assign({name: value.titel}, value)
-      if (newTreeData.children.length < 10000000) newTreeData.children.push(newValue)
+    this.state.tree.projects.map((value, index) => {
+      let newValue = Object.assign({name: value.titel, i: index}, value)
+      if (newTreeData.children.length < 50) newTreeData.children.push(newValue)
     })
 
     console.log(newTreeData)
@@ -50,7 +61,7 @@ class Tree extends Component {
     let treemap = d3.tree()
       .size([width * 0.8, height])
 
-      //  assigns the data to a hierarchy using parent-child relationships
+    //  assigns the data to a hierarchy using parent-child relationships
     let nodes = d3.hierarchy(this.state.treeData)
 
     // maps the node data to the tree layout
@@ -71,14 +82,14 @@ class Tree extends Component {
     // moves the 'group' element to the top left margin
     d3.select('#institution-tree').select('g').remove()
     /* let svg = d3.select('#institution-tree').append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom) */
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom) */
     let g = d3.select('#institution-tree').append('g')
       .attr('transform',
         'translate(' + margin.left + ',' + margin.top + ')')
 
-      // adds the links between the nodes
-      // let link = g.selectAll('.link')
+    // adds the links between the nodes
+    // let link = g.selectAll('.link')
     g.selectAll('.link')
       .data(nodes.descendants().slice(1))
       .enter().append('path')
@@ -87,21 +98,22 @@ class Tree extends Component {
       .attr('d', (d) => {
         let dx = d.x + deltaX
         let dy
-        if (!d.parent) {
+        if (!d.parent) { // root node
           dy = d.y + deltaY
         } else {
           dy = height - treeY
+          if (d.data.i % 2 !== 0) { // results in two lines of projects
+            dy = height - treeY - 30
+          }
         }
         let dParentX = d.parent.x + deltaX
         let dParentY = d.parent.y + deltaY
 
         return 'M' + dx + ',' + dy +
-                  'C' + dx + ',' + (dy + dParentY) / 2 +
-                  ' ' + dParentX + ',' + (dy + dParentY) / 2 +
-                  ' ' + dParentX + ',' + dParentY
+              'C' + dx + ',' + (dy + dParentY) / 2 +
+              ' ' + dParentX + ',' + (dy + dParentY) / 2 +
+              ' ' + dParentX + ',' + dParentY
       })
-
-    console.log(nodes)
 
     // adds each node as a group
     let node = g.selectAll('.node')
@@ -109,7 +121,7 @@ class Tree extends Component {
       .enter().append('g')
       .attr('class', (d) => {
         return 'node' +
-                  (d.children ? ' node--internal' : ' node--leaf')
+                    (d.children ? ' node--internal' : ' node--leaf')
       })
       .attr('transform', (d) => {
         // console.log(d)
@@ -121,12 +133,15 @@ class Tree extends Component {
           dy = d.y + deltaY
         } else {
           dy = height - (rootNodeY + deltaY)
+          if (d.data.i % 2 !== 0) { // results in two lines of projects
+            dy = height - (rootNodeY + deltaY) - 30
+          }
         }
         // console.log(scale)
         // console.log(translation)
 
         return `translate(${dx},${dy}) ${scale}`
-      })
+      }).on('click', this.clickNode)
 
       // adds the circle to the node
     node.append('circle')
@@ -137,12 +152,16 @@ class Tree extends Component {
         return (d.parent ? '#9DBC25' : '#386253')
       }).style('stroke-width', '3px')
 
-      // adds the text to the node
+    // adds the text to the node
     node.append('text')
       .attr('dy', '.35em')
-      .attr('y', (d) => { return d.children ? -20 : 20 })
+      .attr('y', (d) => {
+        return d.children ? -20 : 20
+      })
       .style('text-anchor', 'middle')
-      .text((d) => { if (!d.parent) return d.data.name })
+      .text((d) => {
+        if (!d.parent) return d.data.name
+      })
 
     return (
       <g className={'rsm-tree'} id={'institution-tree'}/>
