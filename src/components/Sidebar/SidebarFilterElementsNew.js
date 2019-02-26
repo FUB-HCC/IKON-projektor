9a273addc7c7c7ddfd2a65177a2bb7a79e89c426
Samiul <msashawon@gmail.com>
2019-02-26 20:17:29 +0600

    fixing filter function

--- src/components/Sidebar/SidebarFilterElementsNew.js
+++ src/components/Sidebar/SidebarFilterElementsNew.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-multi-str */
-import React from 'react'
+import React, {Component} from 'react'
 
 import classes from './SidebarFilterElementsNew.css'
 import {getFieldColor, getTopicColor} from '../../store/utility'
@@ -10,18 +10,18 @@ const SidebarFilterElementsNew = (props) => {
     <div className={classes.Body}>
       <div>
         <div>
-          <CheckBox disable={true} checked={false} name={filters[0].name.toUpperCase()} classes={classes.Title} />
+          <CheckBox show={false} disable={true} checked={false} name={filters[0].name.toUpperCase()} classes={classes.Title} />
         </div>
         <div>
           {filters[0].subGroup.map((subGroup, sgIndex) => {
             const color = getTopicColor(subGroup.name) === '#989aa1' ? getFieldColor(subGroup.name) : getTopicColor(subGroup.name)
             return (
               <div key={sgIndex} >
-                <CheckBox disable={true} checked={false} name={subGroup.name.toUpperCase()} classes={classes.subGroup} color={color} />
+                <CheckBox show={true} disable={false} checked={true} name={subGroup.name.toUpperCase()} classes={classes.subGroup} color={color} change={props.change} id={0} value={subGroup.name} />
                 <div>
                   {subGroup.values.map((name, nIndex) => {
                     return (
-                      <CheckBox disable={false} key={nIndex} checked={true} name={name} classes={classes.subGroup} color={color} change={props.change} id={sgIndex === 3 ? 0 : 1} />
+                      <CheckBox show={true} disable={false} key={nIndex} checked={true} name={name} classes={classes.subGroup} color={color} change={props.change} id={sgIndex === 3 ? 0 : 1} value={name} />
                     )
                   })}
                 </div>
@@ -33,26 +33,39 @@ const SidebarFilterElementsNew = (props) => {
 
       <div>
         <div>
-          <CheckBox disable={true} checked={false} name={filters[1].name.toUpperCase()} classes={classes.Title} />
+          <CheckBox show={false} disable={true} checked={false} name={filters[1].name.toUpperCase()} classes={classes.Title} />
         </div>
         <div>
-          {filters[1].values.map((item, index) => <CheckBox disable={false} key={index} checked={true} name={item} classes={classes.subGroup} color={getTopicColor(item) === '#989aa1' ? getFieldColor(item) : getTopicColor(item)} change={props.change} id={0} />)}
+          {filters[1].values.map((item, index) => <CheckBox show={true} disable={false} key={index} checked={true} name={item} classes={classes.subGroup} color={getTopicColor(item) === '#989aa1' ? getFieldColor(item) : getTopicColor(item)} change={props.change} id={2} value={item} />)}
         </div>
       </div>
     </div>
   )
 }
 
-const CheckBox = (props) => {
-  // console.log(props)
-  const config = {disabled: props.disable}
-  return (
-    <div>
-      <span className={props.classes} style={{color: props.color}} >{ props.name }</span>
-      <input {...config} checked={props.checked} className={classes.CheckBox} type='checkbox' id={props.name} onChange={() => props.change(props.id, props.name, 'a')} />
-      <label className={classes.CheckBoxLabel} htmlFor={props.name} />
-    </div>
-  )
+class CheckBox extends Component {  
+  constructor (props) {
+    super(props)
+    this.state = {
+      checked: this.props.checked
+    }
+    this.onChange = this.onChange.bind(this)
+  }
+  onChange () {
+    this.setState({checked: !this.state.checked})
+    this.props.change(this.props.id, this.props.value, 'a')
+  }
+  render () {
+    const props = this.props
+    const config = {disabled: props.disable}
+    return (
+      <div>
+        <span className={props.classes} style={{color: props.color}} >{ props.name }</span>
+        <input {...config} checked={this.state.checked} className={classes.CheckBox} type='checkbox' id={props.name} onChange={() => this.onChange(props.id, props.name, 'a')} />
+        {props.show && <label className={classes.CheckBoxLabel} htmlFor={props.name} /> }
+      </div>
+    )
+  }
 }
 
 export default SidebarFilterElementsNew
