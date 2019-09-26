7e120ac2803df60b041ef1b039f62e7c52403562
ckinkeldey <christoph@kinkeldey.de>
2019-09-26 15:30:49 +0200

    removed footer

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
             <div className={classes.sidebar}>
               <Sidebar />
             </div>
-            <Footer />
+            {/*<Footer />*/}
           </div>
         </React.Fragment>
       </ConnectedRouter>
