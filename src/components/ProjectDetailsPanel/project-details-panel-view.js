04e80260f990366e44ba544ccdee66ea4f1631d2
Maximilian Eissler <maximilan.eissler@cct-ev.de>
2019-10-01 09:14:48 +0200

    IP-119 added return button for project details panel

--- src/components/ProjectDetailsPanel/project-details-panel-view.js
+++ src/components/ProjectDetailsPanel/project-details-panel-view.js
@@ -1,10 +1,17 @@
 import React from "react";
 import style from "./project-details-panel.module.css";
+import { ReactComponent as Exit } from "../../assets/Exit.svg";
 
 const ProjectDetailsPanel = props => {
   console.log(props);
   return (
     <div className={style.projectDetailsWrapper}>
+      <div
+        className={style.projectDetailsExit}
+        onClick={props.returnToFilterView}
+      >
+        <Exit height={45} width={45} />
+      </div>
       <div className={style.projectDetailsTitle}>
         <span className={style.titleText}>
           {props.projectData.titelprojekt}
