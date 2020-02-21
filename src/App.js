f1fe8eef0edf52d2a497019a747436f719091cc0
mx-e <maxi.eissler@gmail.com>
2020-02-21 18:53:04 +0100

    IP-194 built switch for app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import GraphView from "./pages/GraphView/graph-view";
-import About from "./pages/About/about";
-import Discoveries from "./pages/Discoveries/discoveries";
-import Projects from "./pages/Projects/projects";
 import { default as NavigationSubpages } from "./components/NavigationSubpages/navigation-subpages";
 import Sidebar from "./components/SideBar/sidebar";
 import { ConnectedRouter } from "connected-react-router";
@@ -13,37 +10,49 @@ import classes from "./App.module.css";
 export const menuBarHeight = 65;
 export const appMargin = 5;
 
+const AppBodyStandard = () => (
+  <>
+    <NavigationSubpages height={menuBarHeight} />
+    <div className={classes.appBody}>
+      <div className={classes.contentWindow}>
+        <GraphView />
+      </div>
+      <div className={classes.sidebar}>
+        <Sidebar />
+      </div>
+    </div>
+  </>
+);
+
+const AppBodyTouch = () => (
+  <>
+    <div className={classes.appBody}>
+      <div className={classes.sidebar}>
+        <Sidebar />
+      </div>
+      <div className={classes.contentWindow}>
+        <GraphView />
+      </div>
+    </div>
+  </>
+);
+
 class App extends Component {
   render() {
     return (
       <ConnectedRouter history={history}>
-        <React.Fragment>
+        <>
           <div className={classes.offsetWrapper} style={{ padding: appMargin }}>
-            <NavigationSubpages height={menuBarHeight} />
-            <div className={classes.appBody}>
-              <div className={classes.contentWindow}>
-                <Switch>
-                  <Route
-                    exact
-                    path="/"
-                    render={() => <Redirect to="/explore" />}
-                  />
-                  <Route path="/projects" component={Projects} />
-                  <Route path="/explore" component={GraphView} />
-                  <Route path="/discoveries" component={Discoveries} />
-                  <Route path="/about" component={About} />
-                </Switch>
-              </div>
-              <div className={classes.sidebar}>
-                <Sidebar />
-              </div>
-              {/*<Footer />*/}
-            </div>
+            <Switch>
+              <Route exact path="/" render={() => <Redirect to="/touch" />} />
+              <Route path="/explore" component={AppBodyStandard} />
+              <Route path="/touch" component={AppBodyTouch} />
+            </Switch>
           </div>
-        </React.Fragment>
+        </>
       </ConnectedRouter>
     );
   }
 }
-
+//TODO: reverse redirect
 export default App;
